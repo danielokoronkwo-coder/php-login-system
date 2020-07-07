93158b76071d6a51bfc502f18034da3f5aed0e95
danielokoronkwo-coder <61119104+danielokoronkwo-coder@users.noreply.github.com>
2020-07-07 20:33:17 +0100

    Added SQL query database schema as a comment in schema.php file

---
+++ schema.php
@@ -0,0 +1,13 @@
+<?php
+    /*CREATE TABLE users (
+        id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+        firstname varchar(256) NOT NULL,
+         lastname varchar(256) NOT NULL,
+        email varchar(256) NOT NULL,
+        role varchar(256) NOT NULL,
+        password varchar(256) NOT NULL
+    ) */
+
+
+
+?>
